2640a0eb9e1381c9c2d60a7875517ac13a5d66d3
Harsh Lamba <harshlambamanthanitsolutions@gmail.com>
2025-02-22 14:06:24 +0530

    chanes

--- models/databaseSchema/questionTypes.go
+++ models/databaseSchema/questionTypes.go
@@ -7,14 +7,14 @@ type QuestionType struct {
 }
 
 var DefaultQuestionTypes []QuestionType = []QuestionType{
-	{Id: 1, QuestionType: "star", QuestionTypeLabel: "Star Rating(5)"},
-	{Id: 2, QuestionType: "radio", QuestionTypeLabel: "Radio"},
-	{Id: 3, QuestionType: "select", QuestionTypeLabel: "Dropdown"},
-	{Id: 4, QuestionType: "checkbox", QuestionTypeLabel: "Checkbox"},
-	{Id: 5, QuestionType: "file", QuestionTypeLabel: "File"},
-	{Id: 6, QuestionType: "text", QuestionTypeLabel: "Text"},
-	{Id: 7, QuestionType: "number", QuestionTypeLabel: "Number"},
-	{Id: 8, QuestionType: "textarea", QuestionTypeLabel: "Long Text"},
-	{Id: 9, QuestionType: "phone", QuestionTypeLabel: "Phone Number"},
-	{Id: 10, QuestionType: "email", QuestionTypeLabel: "Email"},
+	{Id: 1, QuestionType: "text", QuestionTypeLabel: "Text"},
+	{Id: 2, QuestionType: "email", QuestionTypeLabel: "Email"},
+	{Id: 3, QuestionType: "textarea", QuestionTypeLabel: "Long Text"},
+	{Id: 4, QuestionType: "number", QuestionTypeLabel: "Number"},
+	{Id: 5, QuestionType: "date", QuestionTypeLabel: "Date"},
+	{Id: 6, QuestionType: "star", QuestionTypeLabel: "Star Rating(5)"},
+	{Id: 7, QuestionType: "radio", QuestionTypeLabel: "Radio"},
+	{Id: 9, QuestionType: "checkbox", QuestionTypeLabel: "Checkbox"},
+	{Id: 8, QuestionType: "select", QuestionTypeLabel: "Dropdown"},
+	{Id: 10, QuestionType: "file", QuestionTypeLabel: "File"},
 }
